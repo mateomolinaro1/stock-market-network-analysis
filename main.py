e3af8a41290b4aeb7ce37b4345e3c984730c4277
mateomolinaro1 <mateo0609@hotmail.fr>
2026-03-11 19:26:33 +0100

    data

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import pandas as pd
+from stock_mkt_network_analysis.utils.config import Config
+config = Config()
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+wrds_mkt_data = pd.read_parquet(
+    config.ROOT_DIR / "data" / "wrds_gross_query.parquet"
+)
+wrds_funda_data = pd.read_parquet(
+    config.ROOT_DIR / "data" / "wrds_funda_gross_query.parquet"
+)
